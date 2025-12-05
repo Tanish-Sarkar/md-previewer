01367fbb529e242b433792818c5cb3d3882c172a
Tanish-Sarkar <tanishnitch@gmail.com>
2025-12-06 00:20:29 +0530

    Setup completed

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,14 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import ReactDOM from "react-dom/client"
+// import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import "katex/dist/katex.min.css";
+import "highlight.js/styles/github-dark.css";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
+
+ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
